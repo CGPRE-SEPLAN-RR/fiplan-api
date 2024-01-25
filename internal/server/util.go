be7d9b4e83bfd7489138bf801866bc33eb52bf91
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-01-25 08:55:21 -0400

    :technologist: adiciona utilitário para ler a query SQL em um arquivo sql

--- internal/server/util.go
+++ internal/server/util.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,6 +11,7 @@ import (
 /*** Erro ***/
 type Erro echo.HTTPError // @name Erro
 
+var ErroLeituraSQL *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao ler o arquivo SQL.")
 var ErroConsultaBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao consultar o banco de dados.")
 var ErroConsultaLinhaBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao consultar uma linha no banco de dados.")
 var ErroRedeOuResultadoBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro de rede ou problema no resultado do banco de dados.")
@@ -46,3 +49,15 @@ var DataTexto map[int]string = map[int]string{
 	DEZEMBRO:  "DEZEMBRO",
 }
 /*** Data ***/
+
+/*** Leitura de Queries ***/
+func LerSQL(path string) (string, error) {
+	content, err := os.ReadFile(fmt.Sprintf("internal/database/queries/%s", path))
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+/*** Leitura de Queries ***/
