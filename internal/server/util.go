50040cc69f3503419b4798acb9d971d6f3d970d0
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-01-26 08:13:31 -0400

    :technologist: adiciona utilitários de erro para template e validação

--- internal/server/util.go
+++ internal/server/util.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,53 +9,19 @@ import (
 /*** Erro ***/
 type Erro echo.HTTPError // @name Erro
 
-var ErroLeituraSQL *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao ler o arquivo SQL.")
+var ErroMontagemTemplate *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao montar o template.")
+var ErroExecucaoTemplate *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao executar o template.")
 var ErroConsultaBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao consultar o banco de dados.")
 var ErroConsultaLinhaBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro ao consultar uma linha no banco de dados.")
 var ErroRedeOuResultadoBancoDados *echo.HTTPError = echo.NewHTTPError(http.StatusInternalServerError, "Ocorreu um erro de rede ou problema no resultado do banco de dados.")
 
+func ErroValidacaoParametro(mensagem []string) *echo.HTTPError {
+	return echo.NewHTTPError(
+		http.StatusBadRequest,
+		map[string][]string{
+			"erros": mensagem,
+		},
+	)
+}
+
 /*** Erro ***/
-
-/*** Data ***/
-const (
-	JANEIRO = iota + 1
-	FEVEREIRO
-	MARCO
-	ABRIL
-	MAIO
-	JUNHO
-	JULHO
-	AGOSTO
-	SETEMBRO
-	OUTUBRO
-	NOVEMBRO
-	DEZEMBRO
-)
-
-var DataTexto map[int]string = map[int]string{
-	JANEIRO:   "JANEIRO",
-	FEVEREIRO: "FEVEREIRO",
-	MARCO:     "MARCO",
-	ABRIL:     "ABRIL",
-	MAIO:      "MAIO",
-	JUNHO:     "JUNHO",
-	JULHO:     "JULHO",
-	AGOSTO:    "AGOSTO",
-	SETEMBRO:  "SETEMBRO",
-	OUTUBRO:   "OUTUBRO",
-	NOVEMBRO:  "NOVEMBRO",
-	DEZEMBRO:  "DEZEMBRO",
-}
-/*** Data ***/
-
-/*** Leitura de Queries ***/
-func LerSQL(path string) (string, error) {
-	content, err := os.ReadFile(fmt.Sprintf("internal/database/queries/%s", path))
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(content), nil
-}
-/*** Leitura de Queries ***/
